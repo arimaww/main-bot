08dda9837f8c3e6a20515114280053d27ceeeb86
Islam <74357802+arimaww@users.noreply.github.com>
2024-09-11 19:39:39 +0300

    adding inline button for opening web app

--- index.ts
+++ index.ts
@@ -1,13 +1,26 @@
-import {config} from 'dotenv'
+import { config } from 'dotenv'
 config()
 import TelegramBot from "node-telegram-bot-api";
 
 
 const token = process.env.TOKEN!;
+const WEB_APP = process.env.WEB_APP!;
 
-const bot = new TelegramBot(token, {polling: true})
+const bot = new TelegramBot(token, { polling: true })
 
 
 bot.on('message', async (msg) => {
-    // code
+    const chatId = msg.chat.id;
+
+    if (msg.text === "/start") {
+        bot.sendMessage(chatId, "Чтобы сделать заказ нажмите на кнопку снизу", {
+            reply_markup: {
+
+                inline_keyboard: [
+                    [{ text: "Открыть каталог", web_app: { url: WEB_APP } }]
+                ]
+            }
+        })
+    }
+
 })
