40b3aff65e45edca4f87fb4c6264272b827da9c7
Islam <74357802+arimaww@users.noreply.github.com>
2024-10-11 21:44:04 +0300

    adding bot start function

--- index.ts
+++ index.ts
@@ -24,6 +24,44 @@ app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 app.options("*", cors())
 
+async function botOnStart() {
+    const orders = await prisma.order.findMany({ where: { status: "WAITPAY" } });
+
+    const seen = new Set();
+    const uniqueOrders = orders.filter(order => {
+        const key = `${order.orderUniqueNumber}-${order.orderUniqueNumber}`
+        const duplicate = seen.has(key);
+        seen.add(key)
+        return !duplicate;
+    })
+    for (const ord of orders) {
+        const prod = await prisma.product.findFirst({ where: { productId: ord.productId! } });
+
+        await prisma.product.update({
+            where: { productId: ord.productId! },
+            data: { count: Number(prod?.count) + Number(ord.productCount) }
+        });
+    }
+    for (const ord of uniqueOrders) {
+        const user = await prisma.user.findFirst({ where: { userId: ord?.userId! } })
+        if (user) {
+            await bot.deleteMessage(user?.telegramId!, parseInt(ord?.messageId!)).catch(err => console.log(err))
+            const keyboard = await prisma.keyboard.findFirst({ where: { chatId: parseInt(user?.telegramId!) } })
+
+            if (keyboard) {
+                await bot.deleteMessage(Number(keyboard?.chatId), Number(keyboard?.messageId)).catch(err => console.log(err))
+                await prisma.keyboard.delete({ where: { keyboardId: keyboard?.keyboardId } })
+            }
+
+            await prisma.order.deleteMany({ where: { orderUniqueNumber: ord?.orderUniqueNumber } })
+        }
+    }
+
+    return await bot.sendMessage(MANAGER_CHAT_ID, "Бот был перезапущен.\nВсе неоплаченные заказы были автоматически удалены.")
+}
+
+botOnStart()
+
 
 const updatingOrdersKeyboard = (orders: Order[], msg: TelegramBot.Message, text: string) => {
     const seen = new Set();
