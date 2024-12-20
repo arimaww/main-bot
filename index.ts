a542e8c20c72f30036a437116c7446715eb71fc6
Islam <74357802+arimaww@users.noreply.github.com>
2024-12-20 14:30:33 +0300

    adding total price with discount

--- index.ts
+++ index.ts
@@ -239,14 +239,14 @@ app.post("/", async (req: Request<{}, {}, TWeb>, res: Response) => {
         const bankId = await prisma.bank
             .findFirst({ where: { bankName: bank } })
             .then((el) => el?.id);
-            const secret = await prisma.secretDiscount.findFirst({
-                where: { id: secretDiscountId },
-            });
+        const secret = await prisma.secretDiscount.findFirst({
+            where: { id: secretDiscountId },
+        });
         if (bankId) {
             for (let prod of uniqueProducts) {
                 const discount = await prisma.productDiscount.findFirst({
                     where: { productId: prod?.productId },
-                }); 
+                });
 
                 await prisma.order.create({
                     data: {
@@ -273,7 +273,9 @@ app.post("/", async (req: Request<{}, {}, TWeb>, res: Response) => {
                         orderType: "CDEK",
                         promocodeId: promocodeId,
                         city: selectedCityName,
-                        secretDiscountPercent: secretDiscountId ? secret?.percent : null,
+                        secretDiscountPercent: secretDiscountId
+                            ? secret?.percent
+                            : null,
                         productCostWithDiscount:
                             Number(prod.cost) * prod.productCount -
                             Number(prod.cost) *
@@ -358,7 +360,9 @@ app.post("/", async (req: Request<{}, {}, TWeb>, res: Response) => {
                                 totalPriceWithDiscount
                                     ? totalPriceWithDiscount
                                     : totalPrice
-                            }\n` + `\Доставка: ${deliverySum} ₽` + `${
+                            }\n` +
+                            `\Доставка: ${deliverySum} ₽` +
+                            `${
                                 secretDiscountId
                                     ? `<blockquote>У данного клиента скидка на ${secret?.percent}%. Корзина сгенерирована менеджером.</blockquote>`
                                     : ""
@@ -701,6 +705,7 @@ async function getOrderData(orderId: string) {
         selectedPvzCode: order?.selectedPvzCode,
         selectedTariff: order?.selectedTariff,
         totalPrice: order?.totalPrice,
+        totalPriceWithDiscount: order?.totalPriceWithDiscount,
         deliveryCost: order?.deliveryCost,
         username: user?.userName,
         selectedCountry: order?.selectedCountry,
@@ -823,7 +828,11 @@ export const handleCallbackQuery = async (query: TelegramBot.CallbackQuery) => {
                     `Трек-номер: ${orderTrackNumberForUser} \n\nПеречень заказа:\n` +
                     `${orderData.products
                         .map((el) => `${el.productCount} шт. | ${el.synonym}`)
-                        .join("\n")}\n\nПрайс: ${orderData?.totalPrice}\n\n` +
+                        .join("\n")}\n\nПрайс: ${
+                        orderData?.totalPriceWithDiscount
+                            ? orderData?.totalPriceWithDiscount
+                            : orderData?.totalPrice
+                    }\n\n` +
                     `Данные клиента:\n` +
                     `${orderData?.surName} ${orderData?.firstName} ${orderData?.middleName}\nГород: ${orderData?.cityName}\n` +
                     `Номер: ${orderData?.phone?.replace(/[ ()-]/g, "")}\n\n` +
@@ -870,7 +879,9 @@ export const handleCallbackQuery = async (query: TelegramBot.CallbackQuery) => {
                                 (el) => `${el.productCount} шт. | ${el.synonym}`
                             )
                             .join("\n")}\n\nПрайс: ${
-                            orderData?.totalPrice
+                            orderData?.totalPriceWithDiscount
+                                ? orderData?.totalPriceWithDiscount
+                                : orderData?.totalPrice
                         }\n\n` +
                         `Данные клиента:\n` +
                         `${orderData?.surName} ${orderData?.firstName} ${orderData?.middleName}\nГород: ${orderData?.cityName}\n` +
