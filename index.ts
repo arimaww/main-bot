08418b3a1156a86ce4e174e96edfd5d2f8812d22
Islam <74357802+arimaww@users.noreply.github.com>
2025-01-09 20:13:55 +0300

    fast1

--- index.ts
+++ index.ts
@@ -28,8 +28,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.options("*", cors());
-app.use(bodyParser.json({ limit: "10mb" })); // Увеличьте лимит для JSON
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(bodyParser.json({ limit: "50mb" })); // Увеличьте лимит для JSON
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 setTimeout(() => botOnStart(bot, MANAGER_CHAT_ID), 5000); // Функция, которая запускается при включении бота или перезагрузки
 
